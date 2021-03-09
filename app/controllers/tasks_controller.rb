6b3a87f5abc31317c1b47495dc124e7b3e671ae6
keisan0601 <angelica.actiloba@icloud.com>
2021-03-09 00:37:28 +0000

    タスク編集ページす済

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,10 +1,15 @@
 class TasksController < ApplicationController
+  before_action :set_user
+  before_action :set_task, only: %i(show edit update destroy)
+  before_action :logged_in_user
+  before_action :correct_user
 
   def index
     @tasks = @user.tasks
   end
 
   def show
+    
   end
 
   def new
